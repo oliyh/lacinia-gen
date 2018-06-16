4f21a46f7c8081d19766a5deb7be31b054df715e
Oliver Hine <git@oliy.co.uk>
2018-06-16 19:23:03 +0100

    bumping cljs

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/test.check "0.10.0-alpha2"]
-                 [com.walmartlabs/lacinia "0.26.0"]
-                 [org.clojure/clojurescript "1.10.238"]]
+                 [com.walmartlabs/lacinia "0.27.0"]
+                 [org.clojure/clojurescript "1.10.312"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-doo "0.1.8"]]
